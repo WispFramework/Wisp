984d949d6f1332efe67aec6dde5b6ac6a7f9b01e
Jakub Sycha <jsycha@jakubsycha.com>
2025-08-12 14:41:51 +0200

    WIP: sessions

--- Wisp.Demo/Controllers/Admin/AdminPostsController.cs
+++ Wisp.Demo/Controllers/Admin/AdminPostsController.cs
@@ -27,48 +27,48 @@ public class AdminPostsController(PostsService ps, FlashService flashService) : 
         return View("admin/posts/new");
     }
 
-    [Authorize("admin")]
-    [Route("/admin/posts/new", "POST")]
-    public async Task<IView> SavePost(IHttpContextAccessor accessor, IAuthenticator authenticator)
-    {
-        var context = await accessor.HttpContext;
-        var formData = context?.Request.FormData;
-        
-        var title = formData?["title"] ?? "";
-        var slug = formData?["slug"] ?? "";
-        var content = formData?["content"] ?? "";
-
-        if (string.IsNullOrWhiteSpace(title))
-        {
-            await flashService.AddFlashMessage("Title is required.", FlashService.FlashMessageType.Error);
-            return Redirect("/admin/posts/new");
-        }
-
-        if (string.IsNullOrWhiteSpace(slug))
-        {
-            await flashService.AddFlashMessage("Slug is required.", FlashService.FlashMessageType.Error);
-            return Redirect("/admin/posts/new");
-        }
-
-        if (string.IsNullOrWhiteSpace(content))
-        {
-            await flashService.AddFlashMessage("Content is required.", FlashService.FlashMessageType.Error);
-            return Redirect("/admin/posts/new");
-        }
-        
-        var user = await authenticator.GetUser();
-        if (user == null)
-        {
-            await flashService.AddFlashMessage("User is not authenticated.", FlashService.FlashMessageType.Error);
-            return Redirect("/admin/posts/new");
-        }
-        
-        var post = new Post
-        {
-            Title = title,
-            Slug = slug,
-            Content = content,
-            Author = user
-        }
-    }
+    // [Authorize("admin")]
+    // [Route("/admin/posts/new", "POST")]
+    // public async Task<IView> SavePost(IHttpContextAccessor accessor, IAuthenticator authenticator)
+    // {
+    //     var context = await accessor.HttpContext;
+    //     var formData = context?.Request.FormData;
+    //     
+    //     var title = formData?["title"] ?? "";
+    //     var slug = formData?["slug"] ?? "";
+    //     var content = formData?["content"] ?? "";
+    //
+    //     if (string.IsNullOrWhiteSpace(title))
+    //     {
+    //         await flashService.AddFlashMessage("Title is required.", FlashService.FlashMessageType.Error);
+    //         return Redirect("/admin/posts/new");
+    //     }
+    //
+    //     if (string.IsNullOrWhiteSpace(slug))
+    //     {
+    //         await flashService.AddFlashMessage("Slug is required.", FlashService.FlashMessageType.Error);
+    //         return Redirect("/admin/posts/new");
+    //     }
+    //
+    //     if (string.IsNullOrWhiteSpace(content))
+    //     {
+    //         await flashService.AddFlashMessage("Content is required.", FlashService.FlashMessageType.Error);
+    //         return Redirect("/admin/posts/new");
+    //     }
+    //     
+    //     var user = await authenticator.GetUser();
+    //     if (user == null)
+    //     {
+    //         await flashService.AddFlashMessage("User is not authenticated.", FlashService.FlashMessageType.Error);
+    //         return Redirect("/admin/posts/new");
+    //     }
+    //     
+    //     var post = new Post
+    //     {
+    //         Title = title,
+    //         Slug = slug,
+    //         Content = content,
+    //         Author = user
+    //     }
+    // }
 }
